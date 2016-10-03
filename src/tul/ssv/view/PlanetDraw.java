64c2ed41f03fc68f4c75d2e10bf46a59d9e5a328
Jan_Hybs <jan.hybs@tul.cz>
2016-10-03 15:39:24 +0200

    Edit paint method

--- src/tul/ssv/view/PlanetDraw.java
+++ src/tul/ssv/view/PlanetDraw.java
@@ -5,8 +5,32 @@ import java.awt.*;
 
 public class PlanetDraw extends JPanel{
 
-    public void paint(Graphics g){
-        g.drawOval(400,300,10,10);
+    public PlanetDraw() {
+        this.setSize(400, 400);
+        this.setBackground(new Color(0, 0, 0, 0));
+    }
+
+    
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        Graphics2D graphics2D = (Graphics2D)g;
+        graphics2D.setColor(Color.red);
+        graphics2D.setRenderingHint(
+                    RenderingHints.KEY_ANTIALIASING, 
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        graphics2D.drawLine(50, 50, 0, 0);
+        graphics2D.fillOval(10,10,100,100);
+        
+        graphics2D.setRenderingHint(
+                    RenderingHints.KEY_ANTIALIASING, 
+                    RenderingHints.VALUE_ANTIALIAS_OFF);
+        
+        graphics2D.drawLine(50, 50, 0, 0);
+        graphics2D.fillOval(120,10,100,100);
     }
 
 }
